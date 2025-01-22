de41feab949285208a75ffc3412d72affc334e57
Giovanni Mauro <giovi.mauro03@gmail.com>
2025-01-22 19:42:31 +0100

    inizialization and starting header

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import Lucy from '../assets/image/lucy_profile.png'
+import Kyou from '../assets/image/kyou_profile.png'
+import Mona from '../assets/image/mona_profile.png'
+
+export default function Header(){
+    return(
+        <header id="hero" className="hero grid gap-4 grid-cols-3">
+            <div className="h-full flex flex-col justify-center items-center gap-4 col-start-1 bg-[#808080]">
+                <img src={Lucy} alt="Foto Lucy" className='w-80' />
+                <p className='text-6xl'>Lucy</p>
+            </div>
+            <div className="h-full flex flex-col justify-center items-center gap-4 col-start-2 bg-[#F2B13B]">
+                <img src={Kyou} alt="Foto Kyou" className='w-80 rotate-[30deg]' />
+                <p className='text-6xl'>Kyou</p>
+            </div>
+            <div className="h-full flex flex-col justify-center items-center gap-4 col-start-3 bg-[#987654]">
+                <img src={Mona} alt="Foto Mona" className='w-80 rotate-[-25deg]' />
+                <p className='text-6xl'>Mona</p>
+            </div>
+        </header>
+    )
+}
