623caa7ab492cb06c6a5e008dece38fc21d18c94
Giovanni Mauro <giovi.mauro03@gmail.com>
2025-01-22 21:30:28 +0100

    navbar layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header.jsx'
+import Navbar from './components/Navbar.jsx'
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <Header />
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
+import { IoMdHome } from "react-icons/io";
+import { FaInstagram } from "react-icons/fa6";
+
 export default function Navbar(){
     return (
-        <nav>
-            
+        <nav className="fixed flex justify-center w-full text-lg">
+            <div className="w-2/3 bg-white flex justify-around items-center py-2 shadow-lg rounded-b-lg">
+                <p><IoMdHome size={25} /></p>
+                <ul className="flex gap-8">
+                    <li>Galleria</li>
+                    <li>Skills</li>
+                    <li>Mamma & Papa'</li>
+                    <li>Contattaci</li>
+                </ul>
+                <p><FaInstagram size={25}/></p>
+            </div>
+
         </nav>
     )
 }
