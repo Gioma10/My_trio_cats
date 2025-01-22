1951b613c16ea2842389e330f95b6791e7cf2263
Giovanni Mauro <giovi.mauro03@gmail.com>
2025-01-22 19:56:26 +0100

    header complete for now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,15 +7,15 @@ export default function Header(){
         <header id="hero" className="hero grid gap-4 grid-cols-3">
             <div className="h-full flex flex-col justify-center items-center gap-4 col-start-1 bg-[#808080]">
                 <img src={Lucy} alt="Foto Lucy" className='w-80' />
-                <p className='text-6xl'>Lucy</p>
+                <p className='text-6xl text-[#575757]'>Lucy</p>
             </div>
             <div className="h-full flex flex-col justify-center items-center gap-4 col-start-2 bg-[#F2B13B]">
                 <img src={Kyou} alt="Foto Kyou" className='w-80 rotate-[30deg]' />
-                <p className='text-6xl'>Kyou</p>
+                <p className='text-6xl text-[#A87F34]'>Kyou</p>
             </div>
             <div className="h-full flex flex-col justify-center items-center gap-4 col-start-3 bg-[#987654]">
                 <img src={Mona} alt="Foto Mona" className='w-80 rotate-[-25deg]' />
-                <p className='text-6xl'>Mona</p>
+                <p className='text-6xl text-[#694F36]'>Mona</p>
             </div>
         </header>
     )
