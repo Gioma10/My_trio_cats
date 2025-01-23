95338b35e02043ad8b8760787d90849531c2d879
Giovanni Mauro <giovi.mauro03@gmail.com>
2025-01-23 17:03:33 +0100

    responsive design for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,40 @@
 import { IoMdHome } from "react-icons/io";
 import { FaInstagram } from "react-icons/fa6";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoIosClose } from "react-icons/io";
+import { useState } from "react";
 
 export default function Navbar(){
+    const[activeHamb, setActiveHamb]= useState(false)
+
+    function handleHamburger(){
+        setActiveHamb(prevHamb => !prevHamb)
+    }
     return (
-        <nav className="fixed flex justify-center w-full text-lg">
-            <div className="w-2/3 bg-white flex justify-around items-center py-2 shadow-lg rounded-b-lg">
-                <p><IoMdHome size={25} /></p>
+        <nav className="fixed flex justify-center w-full text-xs sm:text-sm md:text-lg">
+            <div className="hidden sm:w-5/6 md:w-4/5 lg:w-2/3 bg-white sm:flex justify-around items-center py-2 shadow-lg rounded-b-lg">
+                <p className=" text-xl sm:text-2xl md:text-4xl text-center"><IoMdHome /></p>
                 <ul className="flex gap-8">
                     <li>Galleria</li>
                     <li>Skills</li>
                     <li>Mamma & Papa'</li>
                     <li>Contattaci</li>
                 </ul>
-                <p><FaInstagram size={25}/></p>
+                <p className="text-xl"><FaInstagram/></p>
             </div>
-
+            <div className="sm:hidden w-full bg-white shadow-lg">
+                <button className="text-4xl p-2" onClick={handleHamburger}> {!activeHamb ? <RxHamburgerMenu/> : <IoIosClose />}</button>
+                {activeHamb && (
+                    <div className="bg-black text-white">
+                        <ul className="flex flex-col gap-4 p-5 justify-center items-center w-full">
+                            <li>Galleria</li>
+                            <li>Skills</li>
+                            <li>Mamma & Papa'</li>
+                            <li>Contattaci</li>
+                        </ul>
+                    </div>
+                )}
+            </div>
         </nav>
     )
 }
