4542bb576d51d7cd4b63d82306225fc1fb812672
Giovanni Mauro <giovi.mauro03@gmail.com>
2025-01-23 17:30:18 +0100

    responsive design header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,18 +4,18 @@ import Mona from '../assets/image/mona_profile.png'
 
 export default function Header(){
     return(
-        <header id="hero" className="hero grid gap-4 grid-cols-3">
-            <div className="cat h-full flex flex-col justify-center items-center gap-4 col-start-1 bg-[#808080]">
-                <img src={Lucy} alt="Foto Lucy" className='w-80' />
-                <p className='text-6xl text-[#575757]'>Lucy</p>
+        <header id="hero" className="hero grid gap-4 sm:grid-cols-3 z-0">
+            <div className="cat h-full flex sm:flex-col justify-center items-center gap-4 sm:col-start-1 bg-[#808080]">
+                <img src={Lucy} alt="Foto Lucy" className='w-36 sm:w-52 md:w-80' />
+                <p className='text-4xl md:text-6xl text-[#575757]'>Lucy</p>
             </div>
-            <div className="cat h-full flex flex-col justify-center items-center gap-4 col-start-2 bg-[#F2B13B]">
-                <img src={Kyou} alt="Foto Kyou" className='w-80 rotate-[30deg]' />
-                <p className='text-6xl text-[#A87F34]'>Kyou</p>
+            <div className="cat h-full flex sm:flex-col justify-center items-center gap-4 sm:col-start-2 bg-[#F2B13B]">
+                <img src={Kyou} alt="Foto Kyou" className='w-36 sm:w-52 md:w-80 rotate-[30deg]' />
+                <p className='text-4xl md:text-6xl text-[#A87F34]'>Kyou</p>
             </div>
-            <div className="cat h-full flex flex-col justify-center items-center gap-4 col-start-3 bg-[#987654]">
-                <img src={Mona} alt="Foto Mona" className='w-80 rotate-[-25deg]' />
-                <p className='text-6xl text-[#694F36]'>Mona</p>
+            <div className="cat h-full flex sm:flex-col justify-center items-center gap-4 sm:col-start-3 bg-[#987654]">
+                <img src={Mona} alt="Foto Mona" className='w-36 sm:w-52 md:w-80 rotate-[-25deg]' />
+                <p className='text-4xl md:text-6xl text-[#694F36]'>Mona</p>
             </div>
         </header>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar(){
         setActiveHamb(prevHamb => !prevHamb)
     }
     return (
-        <nav className="fixed flex justify-center w-full text-xs sm:text-sm md:text-lg">
+        <nav className="fixed flex justify-center w-full text-xs sm:text-sm md:text-lg z-10">
             <div className="hidden sm:w-5/6 md:w-4/5 lg:w-2/3 bg-white sm:flex justify-around items-center py-2 shadow-lg rounded-b-lg">
                 <p className=" text-xl sm:text-2xl md:text-4xl text-center"><IoMdHome /></p>
                 <ul className="flex gap-8">
@@ -26,7 +26,7 @@ export default function Navbar(){
                 <button className="text-4xl p-2" onClick={handleHamburger}> {!activeHamb ? <RxHamburgerMenu/> : <IoIosClose />}</button>
                 {activeHamb && (
                     <div className="bg-black text-white">
-                        <ul className="flex flex-col gap-4 p-5 justify-center items-center w-full">
+                        <ul className="flex flex-col gap-4 p-5 justify-center items-center w-full z-10">
                             <li>Galleria</li>
                             <li>Skills</li>
                             <li>Mamma & Papa'</li>
