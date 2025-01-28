ee50ffda17f12032b4043d72e83f4f1a62c88b55
Giovanni Mauro <giovi.mauro03@gmail.com>
2025-01-28 15:31:58 +0100

    filter gallery

---
+++ src/components/FilterGallery.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function FilterGallery(){
+    const [pressBtn, setPressBtn]=useState({
+        lucy: false,
+        kyou: false,
+        mona: false,
+    })
+
+    function handlePressBtn(identifer){
+        setPressBtn(prevBtn => {
+            return {
+                ...prevBtn,
+                [identifer]: !prevBtn[identifer],
+            }
+        })
+        
+    }
+    console.log(pressBtn);
+
+    
+    let classBtn = " bg-white px-4 py-2 rounded-lg border-black border-2";
+    let classBtnL = " bg-[#808080] text-[#575757] px-4 py-2 rounded-lg border-black border-2";
+    let classBtnK = " bg-[#F2B13B] text-[#A87F34] px-4 py-2 rounded-lg border-black border-2";
+    let classBtnM = " bg-[#987654] text-[#694F36] px-4 py-2 rounded-lg border-black border-2";
+    
+
+    return (
+        <div className="pt-20 flex justify-center items-center gap-10">
+            <button onClick={()=>handlePressBtn('lucy')} className={pressBtn.lucy ?  classBtnL : classBtn}>Lucy</button>
+            <button onClick={()=>handlePressBtn('kyou')} className={pressBtn.kyou ? classBtnK : classBtn}>Kyou</button>
+            <button onClick={()=>handlePressBtn('mona')} className={pressBtn.mona ? classBtnM : classBtn}>Mona</button>
+        </div>
+    )
+}
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,9 +1,11 @@
+import FilterGallery from "../components/FilterGallery";
 import Navbar from "../components/Navbar";
 
 export default function Gallery(){
     return(
         <>
             <Navbar path='galleria'/>
+            <FilterGallery />
         </>
     )
 }
